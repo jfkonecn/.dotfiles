679c92be9afc24ed3210e10f934daef617b43e00
John Konecny <24961694+jfkonecn@users.noreply.github.com>
2023-05-28 13:43:45 -0400

    a

--- nvim/.config/nvim/lua/libs/vim-test.lua
+++ nvim/.config/nvim/lua/libs/vim-test.lua
@@ -3,3 +3,6 @@ vim.api.nvim_set_keymap("n", "<leader>T", ":TestFile<CR>", { noremap = true, sil
 vim.api.nvim_set_keymap("n", "<leader>a", ":TestSuite<CR>", { noremap = true, silent = true })
 vim.api.nvim_set_keymap("n", "<leader>l", ":TestLast<CR>", { noremap = true, silent = true })
 vim.api.nvim_set_keymap("n", "<leader>g", ":TestVisit<CR>", { noremap = true, silent = true })
+vim.cmd([[
+let g:test#cpp#catch2#make_command = 'cmake'
+]])
