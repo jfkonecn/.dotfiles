841bd3199c91c01f2b5181e3c88e8a449144ef5a
John Konecny <24961694+jfkonecn@users.noreply.github.com>
2023-10-29 17:45:16 -0400

    a

--- nvim/.config/nvim/lua/libs/dap/csharp.lua
+++ nvim/.config/nvim/lua/libs/dap/csharp.lua
@@ -11,72 +11,25 @@ local function setup()
 	}
 end
 
--- example command
--- dotnet test /home/jfkonecn/repos/csharp-playground/CSharpPlayground.Console.Tests/CSharpPlayground.Console.Tests.csproj --filter FullyQualifiedName=CSharpPlayground.Console.Tests.CodeWarsTests.ToWeirdCaseTest
---
-
 local function runSingleTest(cmd)
-	-- example cmd
-	-- dotnet test /home/jfkonecn/repos/csharp-playground/CSharpPlayground.Console.Tests/CSharpPlayground.Console.Tests.csproj --filter FullyQualifiedName=CSharpPlayground.Console.Tests.CodeWarsTests.ToWeirdCaseTest
-	--
-	--print("running single test")
-	--print("cmd " .. cmd)
-	--print("cwd " .. vim.fn.getcwd())
-	--print("done running single test")
-	--{
-	--type = "coreclr",
-	--name = "launch - netcoredbg",
-	--request = "launch",
-	--program = "${workspaceFolder}/CSharpPlayground.Console/bin/Debug/net6.0/CSharpPlayground.Console.dll",
-	--},
-	--
-	print("VSTEST_HOST_DEBUG=1 " .. cmd)
-	--io.popen("VSTEST_HOST_DEBUG=1 " .. cmd)
-	--io.popen(
-	--"VSTEST_HOST_DEBUG=1 dotnet test /home/jfkonecn/repos/csharp-playground/CSharpPlayground.Console.Tests/CSharpPlayground.Console.Tests.csproj --filter FullyQualifiedName=CSharpPlayground.Console.Tests.CodeWarsTests.ToWeirdCaseTest"
-	--)
-	--local job_id = vim.fn.jobstart(
-	--"VSTEST_HOST_DEBUG=1 dotnet test /home/jfkonecn/repos/csharp-playground/CSharpPlayground.Console.Tests/CSharpPlayground.Console.Tests.csproj --filter FullyQualifiedName=CSharpPlayground.Console.Tests.CodeWarsTests.ToWeirdCaseTest",
-	--{
-	local jodIsRunning = false
-	local job_id = vim.fn.jobstart("VSTEST_HOST_DEBUG=1 " .. cmd, {
+	local jobIsRunning = false
+	vim.fn.jobstart("VSTEST_HOST_DEBUG=1 " .. cmd, {
 		on_stdout = function(_, data, _)
-			print("stdout ", data)
+			if jobIsRunning then
+				return
+			end
 			for key, value in pairs(data) do
 				print(key, value)
 			end
-			if jodIsRunning then
-				return
-			end
 			local pid = tonumber(io.popen("pgrep -n dotnet"):read("*n"))
-			print("pid ", pid)
-			jodIsRunning = true
+
+			print("pid", pid)
+			jobIsRunning = true
 			dap.run({
 				type = "coreclr", -- Replace with your debugger type (e.g., 'csharp', 'dotnet')
 				name = "Run Single .NET Test",
 				request = "attach",
 				processId = pid,
-				--processId = 3477286,
-				--processId = "${command:pickProcess}",
-				--program = "/home/jfkonecn/repos/csharp-playground/CSharpPlayground.Console.Tests/CSharpPlayground.Console.Tests.csproj",
-				--args = { "--filter", "FullyQualifiedName=CSharpPlayground.Console.Tests.CodeWarsTests.ToWeirdCaseTest" },
-				--cwd = "${workspaceFolder}",
-				----stopAtEntry = false,
-				--serverReadyAction = {
-				--action = "openExternally",
-				--pattern = "Now listening on",
-				--},
-				--console = "internalConsole",
-				--pipeTransport = {
-				--pipeProgram = "powershell",
-				--pipeArgs = { "-NoExit", "-Command" },
-				--debuggerPath = "${env:DOTNET_ROOT}/.dotnet/dotnet",
-				--},
-				--logging = {
-				--engineLogging = true,
-				--trace = true,
-				--traceResponse = true,
-				--},
 			})
 		end,
 		on_stderr = function(_, data, _)
@@ -86,31 +39,8 @@ local function runSingleTest(cmd)
 			print("exit code ", exit_code)
 		end,
 	})
-	local pid = vim.fn.jobpid(job_id)
-	----local job_id = vim.fn.jobstart("VSTEST_HOST_DEBUG=1 " .. cmd, {
-	--on_stdout = function(_, data, _)
-	--print("stdout ", data)
-	--end,
-	--on_stderr = function(_, data, _)
-	--print("stderr ", data)
-	--end,
-	--on_exit = function(_, exit_code, _)
-	--print("exit code ", exit_code)
-	--end,
-	--}
-	--)
-	--vim.fn.system("VSTEST_HOST_DEBUG=1 " .. cmd)
-	--vim.fn.jobstart("VSTEST_HOST_DEBUG=1 " .. cmd)
-	print("job_id ", job_id)
-	--vim.api.nvim_command("!VSTEST_HOST_DEBUG=1 " .. cmd .. " &")
-	--vim.api.nvim_command("terminal VSTEST_HOST_DEBUG=1 " .. cmd .. " &")
-	--vim.api.nvim_command("!VSTEST_HOST_DEBUG=1 " .. cmd)
-	--vim.api.nvim_command("!ls")
-	local pid2 = tonumber(io.popen("pgrep -n dotnet"):read("*n"))
-	print("pid ", pid)
-	print("pid2 ", pid2)
-	--local pid = process:read("*n")
-	--print("pid ", pid)
+	-- sleep to wait for the debugable process to start
+	os.execute("sleep 1")
 end
 
 return {
