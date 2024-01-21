db24b4cb753be3e09e6866b701cb09ecd9646a10
John Konecny <24961694+jfkonecn@users.noreply.github.com>
2024-01-21 14:47:43 -0500

    create uuid command

--- nvim/.config/nvim/lua/libs/remaps.lua
+++ nvim/.config/nvim/lua/libs/remaps.lua
@@ -2,3 +2,17 @@
 vim.api.nvim_set_keymap("v", "<leader>y", '"+y', { noremap = true, silent = true })
 vim.api.nvim_set_keymap("n", "<A-k>", ":m-2<CR>", { noremap = true, silent = true })
 vim.api.nvim_set_keymap("n", "<A-j>", ":m+1<CR>", { noremap = true, silent = true })
+
+local function generate_and_copy_uuid()
+	local handle = io.popen("uuidgen | tr -d '\\n'")
+	if handle == nil then
+		print("Failed to generate UUID")
+		return
+	end
+	local result = handle:read("*a")
+	handle:close()
+
+	vim.fn.setreg('"', result)
+end
+
+vim.api.nvim_create_user_command("UuidIntoReg", generate_and_copy_uuid, {})
