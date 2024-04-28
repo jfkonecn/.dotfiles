798e8d67042ebb3c834fe536cfe39890701dbee4
John Konecny <24961694+jfkonecn@users.noreply.github.com>
2024-04-28 08:07:28 -0400

    removed prettier condition

--- nvim/.config/nvim/lua/libs/none-ls.lua
+++ nvim/.config/nvim/lua/libs/none-ls.lua
@@ -43,9 +43,9 @@ null_ls.setup({
 		----end,
 		--}),
 		null_ls.builtins.formatting.prettier.with({
-			condition = function(utils)
-				return utils.root_has_file({ ".prettierrc", ".prettierrc.json", ".prettierrc.mjs" })
-			end,
+			--condition = function(utils)
+			--return utils.root_has_file({ ".prettierrc", ".prettierrc.json", ".prettierrc.mjs" })
+			--end,
 			extra_filetypes = { "astro" },
 		}),
 		null_ls.builtins.diagnostics.markdownlint,
