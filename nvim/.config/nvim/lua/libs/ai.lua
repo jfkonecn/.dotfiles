6c990fae13f6876e15e8e26cc005d6243f376a09
John Konecny <24961694+jfkonecn@users.noreply.github.com>
2024-05-24 08:25:13 -0400

    added ai

--- nvim/.config/nvim/lua/libs/ai.lua
+++ nvim/.config/nvim/lua/libs/ai.lua
@@ -6,6 +6,6 @@ require("gen").prompts["Explain_Code"] = {
 	prompt = "Explain the following $filetype code to me.\n$text",
 }
 
-require("gen").prompts["Vim_Question"] = {
-	Ask = { prompt = "Please answer this neovim question for me, $input:\n$text" },
+require("gen").prompts["Neovim_Question"] = {
+	prompt = "Please answer this neovim question for me, $input:\n$text",
 }
