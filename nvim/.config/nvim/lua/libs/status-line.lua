385628c6d7f1cbc2423332b2bb9ff1c704ccf352
John Konecny <24961694+jfkonecn@users.noreply.github.com>
2024-06-26 12:28:19 -0400

    added marcos to status line

--- nvim/.config/nvim/lua/libs/status-line.lua
+++ nvim/.config/nvim/lua/libs/status-line.lua
@@ -1,3 +1,13 @@
+-- https://github.com/folke/noice.nvim/wiki/Configuration-Recipes
 require("lualine").setup({
 	options = { theme = "onedark" },
+	sections = {
+		lualine_x = {
+			{
+				require("noice").api.statusline.mode.get,
+				cond = require("noice").api.statusline.mode.has,
+				color = { fg = "#ff9e64" },
+			},
+		},
+	},
 })
