4b4ed8984cfb5eb44755b19e9a21189d647240a7
John Konecny <24961694+jfkonecn@users.noreply.github.com>
2024-07-02 15:48:52 -0400

    merge conflicts

--- nvim/.config/nvim/lua/libs/vim-fugitive.lua
+++ nvim/.config/nvim/lua/libs/vim-fugitive.lua
@@ -1,1 +1,3 @@
-vim.keymap.set("n", "<leader>dv", "<cmd>Gdiffsplit!<CR>")
+vim.keymap.set("n", "<leader>dv", "<cmd>Gvdiffsplit!<CR>")
+vim.keymap.set("n", "<leader>h", "<cmd>diffget //2<CR>")
+vim.keymap.set("n", "<leader>l", "<cmd>diffget //3<CR>")
