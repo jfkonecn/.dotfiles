b5bc1da1ee4a7d89de245e8f745ed38adc4dcffe
John Konecny <24961694+jfkonecn@users.noreply.github.com>
2024-08-05 08:50:33 -0400

    rust fmt added back

--- nvim/.config/nvim/lua/libs/none-ls.lua
+++ nvim/.config/nvim/lua/libs/none-ls.lua
@@ -132,3 +132,25 @@ local roc_format = h.make_builtin({
 })
 
 null_ls.register(roc_format)
+
+local rust_format = h.make_builtin({
+	name = "rustfmt",
+	meta = {
+		url = "https://github.com/rust-lang/rustfmt",
+		description = "A tool for formatting rust code according to style guidelines.",
+		notes = {
+			"`--edition` defaults to `2015`. To set a different edition, use `extra_args`.",
+			"See [the wiki](https://github.com/jose-elias-alvarez/null-ls.nvim/wiki/Source-specific-Configuration#rustfmt) for other workarounds.",
+		},
+	},
+	method = FORMATTING,
+	filetypes = { "rust" },
+	generator_opts = {
+		command = "rustfmt",
+		args = { "--emit=stdout" },
+		to_stdin = true,
+	},
+	factory = h.formatter_factory,
+})
+
+null_ls.register(rust_format)
