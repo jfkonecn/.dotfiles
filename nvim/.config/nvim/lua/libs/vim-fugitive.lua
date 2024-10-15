07a0fa24cc47f940c3127e92c3d4d09b8d58c478
John Konecny <24961694+jfkonecn@users.noreply.github.com>
2024-10-15 16:04:21 -0400

    Updated key maps

--- nvim/.config/nvim/lua/libs/vim-fugitive.lua
+++ nvim/.config/nvim/lua/libs/vim-fugitive.lua
@@ -1,3 +1,3 @@
 vim.keymap.set("n", "<leader>dv", "<cmd>Gvdiffsplit!<CR>")
-vim.keymap.set("n", "<leader>h", "<cmd>diffget //2<CR>")
-vim.keymap.set("n", "<leader>l", "<cmd>diffget //3<CR>")
+vim.keymap.set("n", "<leader>gh", "<cmd>diffget //2<CR>")
+vim.keymap.set("n", "<leader>gl", "<cmd>diffget //3<CR>")
