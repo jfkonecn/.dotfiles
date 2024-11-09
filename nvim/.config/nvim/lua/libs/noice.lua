e8f4b4b17757af17b92882bfb60ee6cb9e661560
John Konecny <24961694+jfkonecn@users.noreply.github.com>
2024-11-09 08:25:42 -0500

    removed notifications while editing

--- nvim/.config/nvim/lua/libs/noice.lua
+++ nvim/.config/nvim/lua/libs/noice.lua
@@ -1,9 +1,20 @@
---https://github.com/folke/noice.nvim/wiki/Configuration-Recipes
+-- https://github.com/folke/noice.nvim/wiki/Configuration-Recipes
+-- https://github.com/folke/noice.nvim/wiki/A-Guide-to-Messages
+-- Annoying status stuff issue
+-- https://github.com/folke/noice.nvim/issues/537#issuecomment-1637528605
 require("noice").setup({
+	status = { lsp_progress = { event = "lsp", kind = "progress" } },
 	routes = {
 		{
 			view = "cmdline",
 			filter = { event = "msg_showmode" },
+		},
+		{
+			filter = {
+				event = "lsp",
+				kind = "progress",
+			},
+			opts = { skip = true },
 		},
 	},
 })
--- nvim/.config/nvim/lua/libs/status-line.lua
+++ nvim/.config/nvim/lua/libs/status-line.lua
@@ -4,6 +4,14 @@ require("lualine").setup({
 	sections = {
 		lualine_x = {
 			{
+				function()
+					return require("noice").api.status.lsp_progress.get_hl()
+				end,
+				cond = function()
+					return package.loaded["noice"] and require("noice").api.status.lsp_progress.has()
+				end,
+			},
+			{
 				require("noice").api.statusline.mode.get,
 				cond = require("noice").api.statusline.mode.has,
 				color = { fg = "#ff9e64" },
