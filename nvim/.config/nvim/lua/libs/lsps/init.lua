8f2d29809a935cc703caf7c4478536cd94096af7
John Konecny <24961694+jfkonecn@users.noreply.github.com>
2024-11-09 08:51:29 -0500

    Updated keybindings to match zed

--- nvim/.config/nvim/lua/libs/lsps/init.lua
+++ nvim/.config/nvim/lua/libs/lsps/init.lua
@@ -39,25 +39,25 @@ vim.api.nvim_create_autocmd("LspAttach", {
 		local bufnr = args.buf
 		vim.api.nvim_buf_set_option(bufnr, "omnifunc", "v:lua.vim.lsp.omnifunc")
 
+		-- zed mappings
+		-- https://zed.dev/docs/vim
 		-- Mappings.
 		-- See `:help vim.lsp.*` for documentation on any of the below functions
 		local bufopts = { noremap = true, silent = true, buffer = bufnr }
-		vim.keymap.set("n", "gD", vim.lsp.buf.declaration, bufopts)
-		--vim.keymap.set("n", "gd", vim.lsp.buf.definition, bufopts)
-		--vim.keymap.set("n", "gD", telescopeBuiltin.lsp_definitions, bufopts)
 		vim.keymap.set("n", "gd", telescopeBuiltin.lsp_definitions, bufopts)
-		vim.keymap.set("n", "K", vim.lsp.buf.hover, bufopts)
-		vim.keymap.set("n", "gi", telescopeBuiltin.lsp_implementations, bufopts)
+		vim.keymap.set("n", "gD", vim.lsp.buf.declaration, bufopts)
+		vim.keymap.set("n", "gy", telescopeBuiltin.lsp_type_definitions, bufopts)
+		vim.keymap.set("n", "gI", telescopeBuiltin.lsp_implementations, bufopts)
+		vim.keymap.set("n", "cd", vim.lsp.buf.rename, bufopts)
+		vim.keymap.set("n", "gh", vim.lsp.buf.hover, bufopts)
+		vim.keymap.set("n", "g.", vim.lsp.buf.code_action, bufopts)
+		vim.keymap.set("v", "g.", vim.lsp.buf.code_action, bufopts)
 		vim.keymap.set("n", "<C-k>", vim.lsp.buf.signature_help, bufopts)
 		vim.keymap.set("n", "<leader>wa", vim.lsp.buf.add_workspace_folder, bufopts)
 		vim.keymap.set("n", "<leader>wr", vim.lsp.buf.remove_workspace_folder, bufopts)
 		vim.keymap.set("n", "<leader>wl", function()
 			print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
 		end, bufopts)
-		vim.keymap.set("n", "<leader>D", telescopeBuiltin.lsp_type_definitions, bufopts)
-		vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, bufopts)
-		vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, bufopts)
-		vim.keymap.set("v", "<leader>ca", vim.lsp.buf.code_action, bufopts)
 		vim.keymap.set("n", "gr", telescopeBuiltin.lsp_references, bufopts)
 		vim.keymap.set("n", "<leader>fe", telescopeBuiltin.diagnostics, bufopts)
 		local currentBufferDiagnostics = function()
