d52f432cab689922bb65c561536fb0637783357a
John Konecny <24961694+jfkonecn@users.noreply.github.com>
2025-09-03 13:55:40 -0400

    added bookmarks

---
+++ nvim/.config/nvim/lua/libs/bookmarks.lua
@@ -0,0 +1,6 @@
+local opts = {} -- check the "./lua/bookmarks/default-config.lua" file for all the options
+require("bookmarks").setup(opts) -- you must call setup to init sqlite db
+
+vim.keymap.set({ "n", "v" }, "mm", "<cmd>BookmarksMark<cr>", { desc = "Mark current line into active BookmarkList." })
+vim.keymap.set({ "n", "v" }, "mo", "<cmd>BookmarksGoto<cr>", { desc = "Go to bookmark at current active BookmarkList" })
+vim.keymap.set({ "n", "v" }, "ma", "<cmd>BookmarksCommands<cr>", { desc = "Find and trigger a bookmark command." })
--- nvim/.config/nvim/lua/libs/plugins.lua
+++ nvim/.config/nvim/lua/libs/plugins.lua
@@ -116,4 +116,17 @@ require("lazy").setup({
 	"David-Kunz/gen.nvim",
 
 	"tpope/vim-rhubarb",
+
+	{
+		"LintaoAmons/bookmarks.nvim",
+		-- pin the plugin at specific version for stability
+		-- backup your bookmark sqlite db when there are breaking changes (major version change)
+		tag = "3.2.0",
+		dependencies = {
+			{ "kkharji/sqlite.lua" },
+			{ "nvim-telescope/telescope.nvim" }, -- currently has only telescopes supported, but PRs for other pickers are welcome
+			{ "stevearc/dressing.nvim" }, -- optional: better UI
+			{ "GeorgesAlkhouri/nvim-aider" }, -- optional: for Aider integration
+		},
+	},
 })
