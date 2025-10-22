31b1dfa43c667cba37c4172705108c914a971664
John Konecny <24961694+jfkonecn@users.noreply.github.com>
2025-10-22 07:44:44 -0400

    Added luasnip back

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -133,6 +133,14 @@ require("lazy").setup({
 			{ "GeorgesAlkhouri/nvim-aider" }, -- optional: for Aider integration
 		},
 	},
+	{
+		-- Use this for custom snippets
+		"L3MON4D3/LuaSnip",
+		-- follow latest release.
+		version = "v2.*", -- Replace <CurrentMajor> by the latest released major (first number of latest release)
+		-- install jsregexp (optional!).
+		build = "make install_jsregexp",
+	},
 })
 
 require("libs.mason")
